fc339f6dcbbc0d5170918527d44a8edb9c7cf3e6
Lev Moskaliuk <moskaluklev@gmail.com>
2023-07-24 11:20:07 +0200

    Core exercise completed with added tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,22 +1,24 @@
 import { Player } from "."
+import {Sword} from "./sword";
+import {Axe} from "./axe";
+import {Firespell} from "./firespell";
+import {Icespell} from "./icespell";
 
 describe("Player tests", () => {
     let player: Player
-
+    let sword = new Sword()
+    let axe = new Axe()
+    let firespell = new Firespell()
+    let icespell = new Icespell()
     beforeEach(() => { // Before each "it" test, start with a new Player instance
         player = new Player()
     })
 
-    it("should not use invalid attacks", () => {
-        const result = player.takeHit('banana')
-        expect(result).toEqual('Not a valid attack!')
-    })
-
-    it("should reduce the players health on successful hits", () => {
+    it("should reduce the players health on successful sword hits", () => {
         let result
 
         do {
-            result = player.takeHit('sword')
+            result = player.takeHit(sword)
         } while (result.includes('missed'))
 
         expect(player.health).toBeLessThan(52)
@@ -27,4 +29,53 @@ describe("Player tests", () => {
             expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
         }
     })
+
+    it("should reduce the players health on successful axe hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(axe)
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful fire spell hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(firespell)
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful ice spell hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(icespell)
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
 })
