e246c72360a1bcfca05337e58b295113e0a98733
Marlena Luczak <dziubichmarlena@gmail.com>
2023-07-24 11:46:54 +0200

    core done

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,20 @@
-import { Player } from "."
+import { Player, Sword, Knife, IceSpell } from "."
 
 describe("Player tests", () => {
     let player: Player
+    let sword: Sword;
+    let knife: Knife;
+    let iceSpell: IceSpell;
 
     beforeEach(() => { // Before each "it" test, start with a new Player instance
         player = new Player()
+        sword = new Sword();
+        knife = new Knife();
+        iceSpell = new IceSpell();
     })
 
     it("should not use invalid attacks", () => {
-        const result = player.takeHit('banana')
+        const result = player.takeHit(knife)
         expect(result).toEqual('Not a valid attack!')
     })
 
@@ -16,7 +22,23 @@ describe("Player tests", () => {
         let result
 
         do {
-            result = player.takeHit('sword')
+            result = player.takeHit(sword)
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(iceSpell)
         } while (result.includes('missed'))
 
         expect(player.health).toBeLessThan(52)
