b58c4a07091e7025fb1535a64b9b9eb783e78561
Justyna Dudar <justyna.dudar@talan.com>
2023-07-24 14:28:51 +0200

    implementation for extension Equipment

--- src/Character.ts
+++ src/Character.ts
@@ -1,20 +1,40 @@
 import { AttackType } from "./AttackType"
 
 export class Character {
+
+    private _addedDamage = 0
+    
      constructor(private _health: number, private _armour: number, private _name: string, private _attackType: AttackType){}
     
     get health(): number {
         return this._health
     }
+
     get attackType(): AttackType {
         return this._attackType
+    }
+
+    get armour(): number{
+       return this._armour
+    }
+
+    addArmour(armour: number) {
+        this._armour += armour
+    }
+
+    addDamage(damage: number) {
+        this._addedDamage += damage
+    }
+
+    get addedDamage(): number{
+        return this._addedDamage
     }
 
     takeHit(attackType: AttackType): string {
         let damage: number
 
         try {
-            damage = attackType.attack(this._armour)
+            damage = attackType.attack(this._armour) + this._addedDamage
             this._health -= damage
             return `The attack hit for ${damage} damage! The ${this._name} now has ${this._health} health.`
         } catch (e : any) {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { Character } from './Character'
 import { IceSpell } from './IceSpell'
+import { Equipment } from './Equipment'
 
 export class Player extends Character{
+    private _equipment: Equipment[] = []
+
     constructor(health = 52, armour = 14, name = 'player', attackType = new IceSpell()){
         super(health, armour, name, attackType)
     }
+
+    get equipment(): Equipment[]{
+        return this._equipment
+    }
+
+    addEquipment(equipment: Equipment){
+        this._equipment.push(equipment)
+        this.addArmour(equipment.aromour)
+        this.addDamage(equipment.damage)
+    }
+
 }
 
 
