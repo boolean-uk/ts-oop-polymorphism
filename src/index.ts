5c801004263125290517b352e6d76107ab0b6c53
Kornel <korneldabrowski@gmail.com>
2023-07-24 11:07:31 +0200

    core done

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,113 @@
+interface AttackI {
+  getDamage(): number;
+  getMissMessage(): string;
+  getType(): string;
+}
+
+interface AttackRoll {
+  attackRoll(): number;
+}
+
+class Attack implements AttackRoll {
+  attackRoll(): number {
+    return Math.floor(Math.random() * (20 - 2) + 1);
+  }
+}
+
+class SwordAttack extends Attack implements AttackI {
+  getDamage(): number {
+    return Math.floor(Math.random() * (8 - 2) + 1);
+  }
+
+  getMissMessage(): string {
+    return "The sword attack missed!";
+  }
+
+  getType(): string {
+    return "sword";
+  }
+}
+
+class FireSpellAttack extends Attack implements AttackI {
+  getDamage(): number {
+    return Math.floor(Math.random() * (12 - 2) + 1);
+  }
+
+  getMissMessage(): string {
+    return "The fire spell attack missed!";
+  }
+
+  getType(): string {
+    return "fire spell";
+  }
+}
+
+class IceSpellAttack extends Attack implements AttackI {
+  getDamage(): number {
+    return Math.floor(Math.random() * (12 - 2) + 1);
+  }
+
+  getMissMessage(): string {
+    return "The ice spell attack missed!";
+  }
+
+  getType(): string {
+    return "ice spell";
+  }
+}
+
+class AxeAttack extends Attack implements AttackI {
+  getDamage(): number {
+    return Math.floor(Math.random() * (10 - 2) + 1);
+  }
+
+  getMissMessage(): string {
+    return "The axe attack missed!";
+  }
+
+  getType(): string {
+    return "axe";
+  }
+}
+
 export class Player {
-    private _health = 52 // when this reaches 0, the player dies
-    private _armour = 14 // an attack must be >= this to hit the player
+  private _health = 52; // when this reaches 0, the player dies
+  private _armour = 14; // an attack must be >= this to hit the player
 
-    get health(): number {
-        return this._health
+  get health(): number {
+    return this._health;
+  }
+
+  takeHit(attackType: string): string {
+    const attack = this.getAttack(attackType);
+
+    if (!attack) {
+      return "Not a valid attack!";
     }
 
-    takeHit(attackType: string): string {
-        let damage: number
+    const attackRoll = attack.attackRoll();
 
-        if (attackType === 'sword') {
-            const attackRoll = Math.floor(Math.random() * (20 - 2) + 1) // generate an int between 1 and 20
-
-            if (attackRoll >= this._armour) {
-                damage = Math.floor(Math.random() * (8 - 2) + 1)
-                this._health -= damage
-            } else {
-                return 'The sword attack missed!'
-            }
-        } else if (attackType === 'fire spell') {
-            const attackRoll = Math.floor(Math.random() * (20 - 2) + 1)
-
-            if (attackRoll >= this._armour) {
-                damage = Math.floor(Math.random() * (12 - 2) + 1)
-                this._health -= damage
-            } else {
-                return 'The fire spell attack missed!'
-            }
-        } else if (attackType === 'ice spell') {
-            const attackRoll = Math.floor(Math.random() * (20 - 2) + 1)
-
-            if (attackRoll >= this._armour) {
-                damage = Math.floor(Math.random() * (12 - 2) + 1)
-                this._health -= damage
-            } else {
-                return 'The ice spell attack missed!'
-            }
-        } else if (attackType === 'axe') {
-            const attackRoll = Math.floor(Math.random() * (20 - 2) + 1)
-
-            if (attackRoll >= this._armour) {
-                damage = Math.floor(Math.random() * (10 - 2) + 1)
-                this._health -= damage
-            } else {
-                return 'The axe attack missed!'
-            }
-        } else {
-            return 'Not a valid attack!'
-        }
-
-        return `The attack hit for ${damage} damage! The player now has ${this._health} health.`
+    if (attackRoll >= this._armour) {
+      const damage = attack.getDamage();
+      this._health -= damage;
+      return `The attack hit for ${damage} damage! The player now has ${this._health} health.`;
+    } else {
+      return attack.getMissMessage();
     }
+  }
+
+  private getAttack(attackType: string) {
+    switch (attackType) {
+      case "sword":
+        return new SwordAttack();
+      case "fire spell":
+        return new FireSpellAttack();
+      case "ice spell":
+        return new IceSpellAttack();
+      case "axe":
+        return new AxeAttack();
+      default:
+        return null;
+    }
+  }
 }
