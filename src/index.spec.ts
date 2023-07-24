79de87d0cc51f9e4ed8253437fcacf9c78bbb1e4
Wojciech Żółkowski <wojciechzolkowski@gmail.com>
2023-07-24 11:24:58 +0200

    Added some tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,7 +7,7 @@ describe("Player tests", () => {
         player = new Player()
     })
 
-    it("should reduce the players health on successful hits", () => {
+    it("should reduce the players health on successful sword hits", () => {
         let result
 
         do {
@@ -21,5 +21,80 @@ describe("Player tests", () => {
         if (extractedNumbers !== null) {
             expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
         }
+    })
+
+    it("should reduce the players health on successful axe hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(new AxeAttack())
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful fire spell hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(new FireSpellAttack())
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful ice spell hits", () => {
+        let result
+
+        do {
+            result = player.takeHit(new IceSpellAttack())
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful not defined attack hits", () => {
+        let result
+
+        do {
+            result = player.takeHit({name: "banana", minHit: 1, maxHit:5})
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should return correct statement when missed", () => {
+        let result
+
+        do {
+            result = player.takeHit(new AxeAttack())
+        } while (!result.includes('missed'))
+
+        expect(result).toContain('attack missed')
+
     })
 })
