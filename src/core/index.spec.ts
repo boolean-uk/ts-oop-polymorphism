5d153e9cccd9b40fca54f95904dfb6b75337076a
Alicja Bobko <alicja.bobko@talan.com>
2023-07-24 11:34:44 +0200

    added tests for core

--- src/core/index.spec.ts
+++ src/core/index.spec.ts
@@ -12,11 +12,43 @@ describe("Player tests", () => {
         expect(result).toEqual('Not a valid attack!')
     })
 
-    it("should reduce the players health on successful hits", () => {
+    it("should reduce the players health on successful sword attack", () => {
         let result
 
         do {
             result = player.takeHit('sword')
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on ice spell attack", () => {
+        let result
+
+        do {
+            result = player.takeHit('ice spell')
+        } while (result.includes('missed'))
+
+        expect(player.health).toBeLessThan(52)
+        expect(result).toContain('The attack hit')
+
+        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+        }
+    })
+
+    it("should reduce the players health on successful axe attack", () => {
+        let result
+
+        do {
+            result = player.takeHit('axe')
         } while (result.includes('missed'))
 
         expect(player.health).toBeLessThan(52)
