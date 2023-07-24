2a92544dccb82d2fada23e864327b2b4449ece17
Jakub Pawlowski <jakub.pawloski@talan.com>
2023-07-24 14:15:24 +0200

    Equipment extension done

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,56 @@
-export enum AttackType {
-    Sword = 'sword',
-    FireSpell = 'fire spell',
-    IceSpell = 'ice spell',
-    Axe = 'axe',
+export enum WeaponType {
+    sword = 8,
+    axe = 10,
+    firespell = 12,
+    icespell = 12,
+}
+
+export enum ArmourType {
+    leather = 12,
+    steel = 14,
 }
 
 interface Attack {
-    calculateMiss(damage: number, attackType: string): string
+    calculateMiss(damage: number, weaponType: WeaponType): string
 
-    calculateDamage(attackType: string): number
+    calculateDamage(damage: number): number
 
-    takeHit(attackType: string): string
+    takeHit(weaponType: any): string
 }
 
 export class Player implements Attack {
     private _health = 52
-    private _armour = 14
+    private _armour = ArmourType.steel
 
     get health(): number {
         return this._health
     }
 
-    takeHit(attackType: string): string {
-        if (!Object.values(AttackType).includes(attackType as AttackType)) {
+    takeHit(weaponType: any): string {
+        if (Object.values(WeaponType).includes(weaponType as WeaponType)) {
+            const enumValue = WeaponType[weaponType as keyof typeof WeaponType]
+            const damage = this.calculateDamage(enumValue)
+            this._health -= damage
+            return this.calculateMiss(damage, enumValue)
+        } else {
             return 'Not a valid attack!'
         }
-        const damage = this.calculateDamage(attackType)
-        this._health -= damage
-        return this.calculateMiss(damage, attackType)
     }
 
-    calculateDamage(attackType: string): number {
+    calculateDamage(weaponDamage: number): number {
         let damage: number = 0;
 
         const attackRoll = Math.floor(Math.random() * (20 - 2) + 1)
 
         if (attackRoll >= this._armour) {
-            switch (attackType) {
-                case 'sword':
-                    return damage = Math.floor(Math.random() * (8 - 2) + 1)
-                case 'axe':
-                    return damage = Math.floor(Math.random() * (10 - 2) + 1)
-                case 'ice spell':
-                case 'fire spell':
-                    return damage = Math.floor(Math.random() * (12 - 2) + 1)
-            }
+            damage = Math.floor(Math.random() * (weaponDamage - 2) + 1)
         }
         return damage;
     }
 
-    calculateMiss(damage: number, attackType: string): string {
+    calculateMiss(damage: number, weaponType: WeaponType): string {
         if (damage === 0) {
-            return 'The ${attackType} attack missed!'
+            return 'The ' + weaponType + ' attack missed!'
         }
         return 'The attack hit for ${damage} damage! The player now has ${this._health} health.'
     }
