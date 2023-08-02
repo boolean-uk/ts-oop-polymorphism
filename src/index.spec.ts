a459680bfed6c90af564a826e3acd27d46d552e4
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-08-02 08:22:46 +0200

    core and extension

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,30 +1,78 @@
-import { Player } from "."
+import { Player, Enemy, Sword, IAttack, EnemyType } from ".";
 
-describe("Player tests", () => {
-    let player: Player
+// Core 
+class MockSword implements IAttack {
+    attackType = 'sword';
+    attackRoll = 15;  // over the player's and enemy's armour
+    attackDamage = 5;  // arbitrary
 
-    beforeEach(() => { // Before each "it" test, start with a new Player instance
-        player = new Player()
-    })
+    performAttack(): number {
+        return this.attackDamage;
+    }
+}
 
-    it("should not use invalid attacks", () => {
-        const result = player.takeHit('banana')
-        expect(result).toEqual('Not a valid attack!')
-    })
+// Extension 
+describe("Character tests", () => {
+    let player: Player;
+    let mockSword: MockSword;
 
-    it("should reduce the players health on successful hits", () => {
-        let result
+    beforeEach(() => {
+        player = new Player();
+        mockSword = new MockSword();
+    });
 
-        do {
-            result = player.takeHit('sword')
-        } while (result.includes('missed'))
+    it("should reduce the player's health on successful hits", () => {
+        const result = player.takeHit(mockSword);
 
-        expect(player.health).toBeLessThan(52)
-        expect(result).toContain('The attack hit')
+        expect(player.health).toBeLessThan(52);
+        expect(result).toContain('The sword attack hit');
 
-        const extractedNumbers = result.match(/^\d+|\d+\b|\d+(?=\w)/g) // extract the numbers from the result
+        const extractedNumbers = result.match(/\d+/g); // extract the numbers from the result
         if (extractedNumbers !== null) {
-            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]))
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]));
         }
-    })
-})
+    });
+
+    class MockFireSpell implements IAttack {
+        attackType = 'fire spell';
+        attackRoll = 15;  // over the player's and enemy's armour
+        attackDamage = 5;  // arbitrary
+    
+        performAttack(): number {
+            return this.attackDamage;
+        }
+    }
+
+describe("Enemy tests", () => {
+    let player: Player;
+    let enemy: Enemy;
+    let mockSword: MockSword;
+    let mockFireSpell: MockFireSpell;
+
+    beforeEach(() => {
+        player = new Player();
+        mockSword = new MockSword();
+        mockFireSpell = new MockFireSpell;
+        enemy = new Enemy(60, 10, EnemyType.Warrior, ['fire spell'], mockSword);  // A warrior type enemy resistant to fire spell and having sword as a special attack
+    });
+
+    it("should be resistant to certain attack types", () => {
+        const result = enemy.takeHit(mockFireSpell);
+
+        expect(enemy.health).toBe(60);
+        expect(result).toContain('The fire spell attack was ineffective');
+    });
+
+    it("should reduce the player's health on successful hits", () => {
+        const result = player.takeHit(enemy.specialAttack); // enemy hits with special attack
+
+        expect(player.health).toBeLessThan(52);
+        expect(result).toContain('The sword attack hit');
+
+        const extractedNumbers = result.match(/\d+/g);
+        if (extractedNumbers !== null) {
+            expect(52 - Number(extractedNumbers[0])).toEqual(Number(extractedNumbers[1]));
+        }
+    });
+});
+});
